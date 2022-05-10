266b87a31058bd5f0a33fda3309b55d0ab57da7c
Aslm <aslm.monir@code95>
2022-05-10 23:40:23 +0200

    added Color Manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorManager{
+  static Color primary = const Color(0xffED9728);
+  static Color darkGrey = const Color(0xff232222);
+  static Color grey = const Color(0xff848380);
+  static Color lightGray = const Color(0xffcbcbcb);
+
+}
