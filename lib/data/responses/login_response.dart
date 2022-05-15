01bd09605e4453dda339fc95d72016941a5dc298
Aslm <aslmmon993@gmail.com>
2022-05-15 14:16:03 +0200

    added Data Models

---
+++ lib/data/responses/login_response.dart
@@ -0,0 +1,48 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_response.g.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: 'status')
+  int? status;
+  @JsonKey(name: 'message')
+  int? message;
+
+  BaseResponse(this.status, this.message);
+  factory BaseResponse.fromJson(Map<String, dynamic> json) =>
+      _$BaseResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BaseResponseToJson(this);
+
+}
+
+@JsonSerializable()
+class CustomerResponse {
+  @JsonKey(name: 'id')
+  String? id;
+  @JsonKey(name: 'name')
+  int? name;
+  @JsonKey(name: 'numOfNotifications')
+  int? numOfNotifications;
+
+  CustomerResponse(this.id, this.name, this.numOfNotifications);
+
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) =>
+      _$CustomerResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
+}
+
+@JsonSerializable()
+class AuthResponse extends BaseResponse {
+  @JsonKey(name: 'customer')
+  CustomerResponse? customerResponse;
+
+  AuthResponse(int? status, int? message) : super(status, message);
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
+}
